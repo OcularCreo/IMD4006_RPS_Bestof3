20f7295f5942013375416dd4b3d0ba8b192271ae
Chris <chrishearty2@gmail.com>
2023-10-07 19:06:50 -0400

    Base Scene Setup
    Added Character and Platform prefabs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello World");
+        
     }
 
     // Update is called once per frame
