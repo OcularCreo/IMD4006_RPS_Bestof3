2f36671480477810c64d85947ca214e7b2b777ef
hujillian <hujillian123@gmail.com>
2023-10-09 12:24:13 -0400

    Added movement for player 1

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Presets;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Rigidbody2D rb; // the player rigid body
+    private float moveSpeed = 3f; // speed moving left and right
+    private float jumpSpeed = 5f; // speed of jump
+
+    public bool jumping = false; // check whether the player has jumped
+    public int jumpCount = 0; // counter for double jump
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true; // freeze rotation of the player, because otherwise the circle rotates when it moves
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Player 1
+        // If player presses a, go left
+        if (Input.GetKeyDown("a"))
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0);
+        }
+        // if player presses d, go right
+        if (Input.GetKeyDown("d"))
+        {
+            rb.velocity = new Vector2(moveSpeed, 0);
+        }
+        // if player presses w and player has not jumped twice yet, let player jump
+        if (Input.GetKeyDown("w") && !(jumping))
+        {
+            rb.velocity = Vector2.up * jumpSpeed;
+            jumpCount += 1;
+            jumping = checkJump();
+        }
+
     }
+
+    // check if the player has jumped twice
+    private bool checkJump()
+    {
+        if(jumpCount >= 2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // check if the player has collided with the platform
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if collided, allow the player to jump again
+        if (collision.gameObject.tag == "Platform")
+        {
+            // reset jump variables
+            jumping = false;
+            jumpCount = 0;
+        }
+        //Debug.Log("collision with " + collision.gameObject.name);
+    }
+
 }
