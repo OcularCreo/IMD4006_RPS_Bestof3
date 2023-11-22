53bb111089b42bea8b86358023a8bd3a2e93337d
christmaspranommit <christmaspranommit@cmail.carleton.ca>
2023-11-22 11:23:27 -0500

    Clean up Alert code

--- Assets/Scripts/Alert.cs
+++ Assets/Scripts/Alert.cs
@@ -28,8 +28,6 @@ public class Health : MonoBehaviour
        alertBlinkstart = 0f; //seconds
        alertEvery = 0.4f; // seconds
 
-    //    healthBarNum = health_bar.GetComponent<Transform>().localScale.x;
-    //    healthBarLocalPosition = health_bar.GetComponent<Transform>().position.x;
 
        
     }
@@ -40,23 +38,9 @@ public class Health : MonoBehaviour
       
         health = gameObject.GetComponent<Combat>().health;
         lives = gameObject.GetComponent<Combat>().lives;
-        //Debug.Log(gameObject.name + " health is: " + health);
-
-        //health_alert();
-
-        //lives_aleart();
+       
 
         isHit = gameObject.GetComponent<Combat>().alreadyHit;
-
-
-        // if(isHit){
-            
-        //     healthBarNum -= (1 - (health/100f));
-        //     healthBarLocalPosition =  (health/100f);
-        //     health_bar.GetComponent<Transform>().localScale = new Vector2(health_bar.GetComponent<Transform>().localScale.x-0.3,health_bar.GetComponent<Transform>().localScale.y);
-        //     health_bar.GetComponent<Transform>().position = new Vector2(GetComponent<Transform>().position.x,health_bar.GetComponent<Transform>().position.y);
-        //     Debug.Log(gameObject.name + "is hit" + healthBarNum);
-        // }
        
 
     }
