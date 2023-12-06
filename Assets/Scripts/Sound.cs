157772157440a29cc0236004aa972bc012bac5ee
christmaspranommit <christmaspranommit@cmail.carleton.ca>
2023-12-06 15:18:46 -0500

    Add Audio

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    [SerializeField] public AudioClip sound_background_menu;
+    [SerializeField] public AudioClip sound_background_fight;
+    [SerializeField] public AudioClip sound_background_rps;
+
+    [SerializeField] public AudioClip soundEffect_punch;
+    [SerializeField] public AudioClip soundEffect_jump;
+    [SerializeField] public AudioClip soundEffect_slam;
+    [SerializeField] public AudioClip soundEffect_fly;
+    [SerializeField] public AudioClip soundEffect_drop;
+
+
+    [SerializeField] public AudioClip soundAnnounce_1;
+    [SerializeField] public AudioClip soundAnnounce_2;
+    [SerializeField] public AudioClip soundAnnounce_3;
+
+    
+}
