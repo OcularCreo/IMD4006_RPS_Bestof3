e2ff3e0a10ebfac68f177af78e74b9a67ef35a09
hujillian <hujillian123@gmail.com>
2023-12-07 02:12:36 -0500

    Moving platforms start
    Press t and g on the keyboard to make the trex platform rotate

--- Assets/Scripts/Platforms_Moving.cs
+++ Assets/Scripts/Platforms_Moving.cs
@@ -12,32 +12,34 @@ public class Platforms_Moving : MonoBehaviour
     {
         trexPlatform = GameObject.Find("trex");
         trexPlatform_mouthOpen = GameObject.Find("trex mouth open");
-        trexPlatform_mouthOpen.GetComponent<Renderer>().enabled = false;
-        // fix invisible box collider
+        trexPlatform_mouthOpen.SetActive(false); // set second platform to inactive at start
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if press down t on keyboard, play animation and change to trex mouth open
         if (Input.GetKeyDown("t"))
         {
             StartCoroutine(movePlatform(trexPlatform, trexPlatform_mouthOpen, 30, -1));
         }
+        // if press down g, play reverse animation and change back to trex
         if (Input.GetKeyDown("g"))
         {
             StartCoroutine(movePlatform(trexPlatform_mouthOpen, trexPlatform, 30, 1));
         }
     }
 
+    // function to rotate the platform, then switch to the other image of the platform
     private IEnumerator movePlatform(GameObject platform1, GameObject platform2, int rotateAmount, int rotateDirection)
     {
         for (int i = 0; i < rotateAmount; i++)
         {
-            platform1.transform.Rotate(0, 0, rotateDirection, Space.Self);
+            platform1.transform.Rotate(0, 0, rotateDirection, Space.Self); // rotate visible platform
             yield return new WaitForSeconds(0.01f);
         }
-        platform1.GetComponent<Renderer>().enabled = false;
-        platform1.transform.Rotate(0, 0, -(rotateDirection) * rotateAmount, Space.Self);
-        platform2.GetComponent<Renderer>().enabled = true;
+        platform1.SetActive(false); // set visible platform to inactive
+        platform1.transform.Rotate(0, 0, -(rotateDirection) * rotateAmount, Space.Self); // set platform to initial position
+        platform2.SetActive(true); // set the other (invisible) platform to active
     }
 }
