11746176ce395d06b12af69e3f41a78e5b101c80
hujillian <hujillian123@gmail.com>
2023-12-08 02:34:03 -0500

    Added more platform movement

--- Assets/Scripts/Platforms_Moving.cs
+++ Assets/Scripts/Platforms_Moving.cs
@@ -8,8 +8,10 @@ public class Platforms_Moving : MonoBehaviour
     GameObject trexPlatform;
     GameObject trexPlatform_mouthOpen;
 
+    GameObject crocPlatform;
+
     private bool changed;
-    private bool platformChanged;
+    private bool crocChanged;
 
     [SerializeField] private Manager gameManager;
 
@@ -20,28 +22,20 @@ public class Platforms_Moving : MonoBehaviour
         trexPlatform_mouthOpen = GameObject.Find("trex mouth open");
         trexPlatform_mouthOpen.SetActive(false); // set second platform to inactive at start
 
+        crocPlatform = GameObject.Find("croc");
+
         changed = false;
-        platformChanged = false;
+        crocChanged = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //// if press down t on keyboard, play animation and change to trex mouth open
-        //if (Input.GetKeyDown("t"))
-        //{
-        //    StartCoroutine(movePlatform(trexPlatform, trexPlatform_mouthOpen, 30, -1));
-        //}
-        //// if press down g, play reverse animation and change back to trex
-        //if (Input.GetKeyDown("g"))
-        //{
-        //    StartCoroutine(movePlatform(trexPlatform_mouthOpen, trexPlatform, 30, 1));
-        //}
-
+        
         // only have platform movement in battle state
         if (!changed)
         {
-            // once every 25 seconds, and not at the start of time
+            // trex animation once every 25 seconds, and not at the start of time
             if (Mathf.RoundToInt(Time.time) % 25 == 0 && Mathf.RoundToInt(Time.time) > 0)
             {
                 // if original trex used, play animation and change to trex mouth open platform
@@ -49,7 +43,6 @@ public class Platforms_Moving : MonoBehaviour
                 //{
                     //StartCoroutine(movePlatform(trexPlatform, trexPlatform_mouthOpen, 30, -1));
                     StartCoroutine(movePlatformFull(trexPlatform, trexPlatform_mouthOpen, 30, -1));
-                    platformChanged = true; // delete this bool if changeanimationfull works
                     changed = true;
                 //}
                 //else // otherwise play reverse animation and change back to trex platform
@@ -59,12 +52,27 @@ public class Platforms_Moving : MonoBehaviour
                 //    changed = true;
                 //}
             }
-            
         }
         else if (Mathf.RoundToInt(Time.time) % 25 == 1)
         {
             // 1 second later, set changed back to false
             changed = false;
+        }
+
+        // CROC
+        if (!crocChanged)
+        {
+            // once every 35 seconds, croc animation
+            if (Mathf.RoundToInt(Time.time) % 35 == 0 && Mathf.RoundToInt(Time.time) > 0)
+            {
+                StartCoroutine(translateHorizontalPlatform(crocPlatform, 50));
+                crocChanged = true;
+            }
+        }
+        else if (Mathf.RoundToInt(Time.time) % 35 == 1)
+        {
+            // 1 second later, set changed back to false
+            crocChanged = false;
         }
     }
 
@@ -86,5 +94,21 @@ public class Platforms_Moving : MonoBehaviour
         StartCoroutine(movePlatform(platform1, platform2, rotateAmount, rotateDirection));
         yield return new WaitForSeconds(5.0f);
         StartCoroutine(movePlatform(platform2, platform1, rotateAmount, -(rotateDirection)));
+    }
+
+    private IEnumerator translateHorizontalPlatform(GameObject platform, int translateAmount)
+    {
+        // move slowly to the left
+        for(int i = 0; i < translateAmount; i++)
+        {
+            platform.transform.Translate(-0.1f, 0, 0);
+            yield return new WaitForSeconds(0.05f);
+        }
+        // move fast to the right
+        for (int i = 0; i < translateAmount; i++)
+        {
+            platform.transform.Translate(0.1f, 0, 0);
+            yield return new WaitForSeconds(0.001f);
+        }
     }
 }
